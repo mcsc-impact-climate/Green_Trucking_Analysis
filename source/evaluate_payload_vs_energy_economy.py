3f13be4f844577fe3287e6663783270011eadc32
Danika MacDonell <danikam@mit.edu>
2024-10-01 11:48:58 -0700

    Updates to include additional info in csv file produced by evaluate_payload_vs_energy_economy.py

--- source/evaluate_payload_vs_energy_economy.py
+++ source/evaluate_payload_vs_energy_economy.py
@@ -9,6 +9,7 @@ import truck_model_tools
 import pandas as pd
 from common_tools import get_linear_drivecycles
 from common_tools import make_title_string
+from common_tools import get_nacfe_results
 
 KG_PER_KILOTON = 1e6
 KG_PER_TON = 1000
@@ -17,26 +18,6 @@ KG_PER_LB = 0.453592
 ###################################### Select drivecycles to consider #####################################
 drivecycles = get_linear_drivecycles()
 ###########################################################################################################
-
-def get_nacfe_results(truck_name, drivecycle):
-    """
-    Get NACFE results for the given truck and drivecycle
-    """
-    # Collect the battery info extracted from the NACFE data for each truck
-    battery_capacity_df = pd.read_csv('data/pepsi_semi_battery_capacities.csv')
-    
-    # Collect the info extracted from the drivecycle
-    drivecycle_data_df = pd.read_csv(f'data/{truck_name}_drivecycle_data.csv', index_col='Driving event')
-    
-    # Collect NACFE results
-    NACFE_results = {
-        'Battery capacity (kWh)': battery_capacity_df[truck_name].iloc[0],
-        'Battery capacity unc (kWh)': battery_capacity_df[truck_name].iloc[1],
-        'Fuel economy (kWh/mi)': drivecycle_data_df['Fuel economy (kWh/mile)'].loc[drivecycle],
-        'Fuel economy unc (kWh/mi)': drivecycle_data_df['Fuel economy unc (kWh/mile)'].loc[drivecycle],
-    }
-    
-    return NACFE_results
 
 def evaluate_linear_params_diesel():
     """
@@ -59,6 +40,7 @@ def evaluate_linear_params_diesel():
             slope_gpm, y_intercept_payload_gpm, miles_per_gallon_ave_payload = truck_model_tools_diesel.truck_model(parameters).get_linear_gpm_coefs(df_drivecycle)
             
             result = {
+                'Truck': [make_title_string(truck_name)],
                 'Linear Slope (gal/mile/kiloton)': [slope_gpm * KG_PER_KILOTON],
                 'Y Intercept (gal/mile)': [y_intercept_payload_gpm]
             }
@@ -90,26 +72,29 @@ def evaluate_linear_params_ev():
     parameters.m_ave_payload = 10000 * KG_PER_LB
     battery_params_dict = data_collection_tools.read_battery_params()
 
-    evaluated_linear_params_df = pd.DataFrame(columns=['Truck', 'Linear Slope (kWh/mile/lb)', 'Y Intercept (kWh/mile)', 'Predicted Payload (lb)', 'Predicted GVW (lb)'])
+    evaluated_linear_params_df = pd.DataFrame(columns=['Truck', 'Linear Slope (kWh/mi/kiloton)', 'Y Intercept (kWh/mi)', 'Predicted Payload (lb)', 'Predicted GVW (lb)'])
+    battery_capacity_info = pd.read_csv('data/pepsi_semi_battery_capacities.csv', index_col='Value')
     for truck_name in drivecycles:
         for drivecycle in drivecycles[truck_name]:
             # Read in the NACFE results
             NACFE_results = get_nacfe_results(truck_name, drivecycle)
             
             df_drivecycle = truck_model_tools.extract_drivecycle_data(f'data/{truck_name}_drive_cycle_{drivecycle}.csv')
-            slope_per_kg, y_intercept_payload, kwh_per_mile_ave_payload = truck_model_tools.truck_model(parameters).get_linear_energy_economy_coefs(df_drivecycle, battery_params_dict['Roundtrip efficiency'], battery_params_dict['Energy density (kWh/ton)'], NACFE_results['Battery capacity (kWh)'])
+            slope_per_kg, y_intercept_payload, kwh_per_mile_ave_payload = truck_model_tools.truck_model(parameters).get_linear_energy_economy_coefs(df_drivecycle, battery_params_dict['Roundtrip efficiency'], battery_params_dict['Energy density (kWh/ton)'], battery_capacity_info[truck_name]['Mean'])
             
             slope_per_lb = slope_per_kg * KG_PER_LB
             slope_per_kiloton = slope_per_kg * KG_PER_KILOTON
             
             predicted_payload = ( NACFE_results['Fuel economy (kWh/mi)'] - y_intercept_payload ) / slope_per_lb
-            m_bat = NACFE_results['Battery capacity (kWh)'] * KG_PER_TON / battery_params_dict['Energy density (kWh/ton)']
+            m_bat = battery_capacity_info[truck_name]['Mean'] * KG_PER_TON / battery_params_dict['Energy density (kWh/ton)']
             predicted_gvw = predicted_payload + (parameters.m_truck_no_bat + m_bat) / KG_PER_LB
             
             result = {
                 'Truck': [make_title_string(truck_name)],
-                'Linear Slope (kWh/mile/lb)': [slope_per_kiloton],
-                'Y Intercept (kWh/mile)': [y_intercept_payload],
+                'Energy Economy (kWh/mi)': [NACFE_results['Fuel economy (kWh/mi)']],
+                'Range (miles)': [NACFE_results['Range (miles)']],
+                'Linear Slope (kWh/mi/kiloton)': [slope_per_kiloton],
+                'Y Intercept (kWh/mi)': [y_intercept_payload],
                 'Predicted Payload (lb)': [predicted_payload],
                 'Predicted GVW (lb)': [predicted_gvw]
             }
@@ -124,7 +109,7 @@ def evaluate_linear_params_ev():
     
 
 def main():
-    #evaluate_linear_params_diesel()
+    evaluate_linear_params_diesel()
     evaluate_linear_params_ev()
     
 if __name__ == '__main__':
